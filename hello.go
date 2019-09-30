386298ede8c0ebc2cdd2745b36ad0381b95d83eb
SwanHub <jdprince555@gmail.com>
2019-09-30 11:48:59 -0600

    import lib

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main 
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/golang/example/stringutil"
+)
+// note this package was taken from another space in the go workspace. 
 
 func main(){
-	fmt.Println("Hello, worldo!")
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
 }
